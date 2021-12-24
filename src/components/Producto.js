ce3083dbb0b0895236e9ca93570584d2ae9b05c7
Mario <mamerida2013@gmail.com>
2021-12-24 11:42:46 -0300

    creacion de las tarjetas de los productos del carrito agregando estilos inline

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,16 +1,30 @@
 import { Component } from "react";
+
+const styles = {
+    producto:{
+        border:'solid 2px #eee',
+        boxShadow:'0 5px 5px rgb(0,0,0, 0.1)', // rgb 3 elementos define el color cuarto elemento opacidad
+        width:'30%', // ocupa un 30% del total de la pantalla
+        padding:"10px 15px", // para alejarlo un poco mas de los bordes
+        borderRadius:"5px", //redondear borde
+    },
+    // para que las imagenes tengan su propio estilo y se vean mejor les vamos a dar un estilo 
+    img:{
+      width:"100%"  
+    }
+
+}
+
 // creo el producto de manera individual 
 class Producto extends Component{
-
-    
 
     render(){
         // creo la variable de producto para poder acceder a las propiedades del mismo
         const {producto } = this.props
         console.log(this.props)
         return(
-            <div>
-                <img alt ={producto.name} src = {producto.img}/>
+            <div style={styles.producto}>
+                <img alt ={producto.name} src = {producto.img} style={styles.img}/>
                 <h3>{producto.name}</h3>
                 <p>{producto.price}</p>
             </div>
--- src/components/Productos.js
+++ src/components/Productos.js
@@ -2,6 +2,16 @@ import { Component } from "react";
 import Producto from "./Producto.js"
 
 
+//creo los estilos para mostrarlos en pantalla por inline Style
+
+const styles  ={
+    productos:{
+        display:'flex',
+        flexDirectio:'row',// esto se hace para poder mostrar los elementos de izquierda a derecha  
+        justifyContent:'space-between', //Comparte el 100% del espacio de manera equitativa con los productos 
+    }
+}
+
 // creo el componente de productos, le paso las propiedades que recibe de app en el momento de la instaciacion 
 class Productos extends Component{
     render(){
@@ -9,7 +19,7 @@ class Productos extends Component{
         return(
             //por cada producto pasado al momento de instanciar productos genero un map ( simil forEach) y genero un componente con una funcion una key y un objeto producto
             // que se va a pasar al componente producto para seguir trabajando con el prodcuto
-            <div>
+            <div style={styles.productos}>
                 {/* llamo por cada instancia de productos al producto completo, esto para poder tener un manejo de los productos en general
                 + */}
                 {productos.map((producto)=>
